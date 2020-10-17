b9f8530459a05260b4ee8c23f14f9baed7e5b13a
IndyRoseGee <66797225+IndyRoseGee@users.noreply.github.com>
2020-10-17 12:38:05 +0100

    added formatted date, wind and humidity

---
+++ src/Date.js
@@ -0,0 +1,48 @@
+
+import React from "react";
+
+export default function TodayDate(props) {
+  
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  
+  let day = days[props.date.getDay()];
+
+   let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[props.getMonth()]
+
+  let year = props.date.getFullYear();
+  let date = props.getDate();
+  
+  let hours = props.date.getHours(0, 0, 0, 0);
+  
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+return (
+            <div>
+                Today {hours}:{minutes} | {day} {date} {month} {year}
+      </div>
+      );
+}
--- src/TodayTemp.js
+++ src/TodayTemp.js
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import "./todaytemp.css";
 import bigEmoji from "./media/01d.png";
+import Date from "./Date";
+import Axios from "axios";
 
 export default function TodayTemp() {
     const [ready, setReady] = useState(false);
-    const[temperature, setTemperature] = useState(null);
-    function fandleResponse(response) {
+    const[weatherData, setWeatherData] = useState({});
+    function handleResponse(response) {
         setTemperature(response.data.main.temp);
-        setReady(true);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            date: new Date(response.data.dt * 1000),
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            city: response.data.name,
+        })
     }
 
     if (ready) {
     return (
         <div className="today-temp">
             <p className="today" id="todays-date">
-                Today Time | Day Date Month 2020
-      </p>
+                <Date/>
+             </p>
             <div className="mainTemp">
                 <div className="row">
                     <div className="col-2"></div>
                     <div className="col-3">
                         <span className="maintemp" id="main-temperature">
-    {temperature}{" "}
+                            {Math.round(temperature)}{" "}
                         </span>
                         <span className="maindegrees">
                             <a href="#/" className="active" id="celsius-link">
@@ -50,16 +61,14 @@ export default function TodayTemp() {
             </div>
             <div className="weatherDes">
                 <p>
-                    <span id="desWea">It is Sunny</span>
+                    {weatherData.description}
                 </p>
             </div>
 
             <div className="descriptives">
-                <p className="descriptive">
-                    Humidity = <span id="humidity">-</span>%
+               <p> {weatherData.humidity}%
         </p>
-                <p className="descriptive">
-                    Wind Speed = <span id="wind">-</span> m/s
+             <p>   {weatherData.wind} m/s
         </p>
             </div>
         </div>
@@ -67,5 +76,8 @@ export default function TodayTemp() {
 
 } else {
     const apiKey = "5e479c0f3c564141872dc35a4d10e84c";
-    
-}
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    Axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."; }}
